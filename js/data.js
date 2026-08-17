55b6356dbf214e268425ecb1ad51e2798abe1b4a
Shubham Srivastava <shubhamsri2257@gmail.com>
2026-08-18 02:04:54 +0530

    chart canvat addition

---
+++ js/data.js
@@ -0,0 +1,8 @@
+const chartDataOne = [
+  { month: "Jan", value: 18.2 },
+  { month: "Feb", value: 19.1 },
+  { month: "Mar", value: 20.2 },
+  { month: "Apr", value: 21.8 },
+  { month: "May", value: 23.4 },
+  { month: "Jun", value: 24.8 },
+];
